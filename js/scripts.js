7b6da75d2fa9cafbe9b77b382dcbbb655d37e25a
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-14 14:04:21 -0700

    reverts JS to simple logic for site, updates questions with conent.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,3 @@
-
-function tally(answer1, answer2, answer3, answer4, answer5) {
-  count = [answer1,answer2,answer3,answer4,answer5];
-  return count;
-  alert return;
-}
-
 $(document).ready(function() {
   $(".btn").click(function() {
     $("#programing_quiz").show();
@@ -12,13 +5,7 @@ $(document).ready(function() {
   
   $("form#programing_quiz").submit(function(event) {
     const nameInput = $("#name").val();
-    const answer1 = parseInt($("#question1").val());
-    const answer2 = parseInt($("#question2").val());
-    const answer3 = parseInt($("#question3").val());
-    const answer4 = parseInt($("#question4").val());
-    const answer5 = parseInt($("#question5").val());
-    const result = tally(answer1, answer2, answer3, answer4, answer5);
-
+    const result = parseInt($("#question1").val());
    
     $(".name").append(nameInput);
 
