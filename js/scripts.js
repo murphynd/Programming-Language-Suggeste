d701e86c1666cc1cc0ee2f41964a62272e150846
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-14 13:12:41 -0700

    attempting more complex loigc in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+
+function tally(answer1, answer2, answer3, answer4, answer5) {
+  count [1] = [answer1,answer2,answer3,answer4,answer5];
+  count [2] = [answer1,answer2,answer3,answer4,answer5];
+  count [3] = [answer1,answer2,answer3,answer4,answer5];
+  return count;
+}
+
 $(document).ready(function() {
   $(".btn").click(function() {
     $("#programing_quiz").show();
@@ -5,19 +13,14 @@ $(document).ready(function() {
   
   $("form#programing_quiz").submit(function(event) {
     const nameInput = $("#name").val();
-    const answer = parseInt($("#question").val());
-   
-    $(".name").append(nameInput);
+    const answer1 = parseInt($("#question1").val());
+    const answer2 = parseInt($("#question2").val());
+    const answer3 = parseInt($("#question3").val());
+    const answer4 = parseInt($("#question4").val());
+    const answer5 = parseInt($("#question5").val());
+    const result = tally(answer1, answer2, answer3, answer4, answer5);
+    alert(result)
 
-    if(answer === 1) {
-    $("#language_1").show();
-    } 
-    else if (answer === 2) {
-    $("#language_2").show();
-    } 
-    else if (answer === 3) {
-    $("#language_3").show();
-    }
     event.preventDefault();
   });
 });
