91801ce8c7d573c53efed647dc9521e52d554d05
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-14 16:17:32 -0700

    this will add Try Again button as new function in scrips

--- js/scripts.js
+++ js/scripts.js
@@ -10,27 +10,23 @@ $(document).ready(function() {
     $(".name").append(nameInput);
 
     if(result === 1) {
-
-
     $("#language_1").show();
     $("#programing_quiz").hide();
-    $(".btn").click(reset());
     } 
     else if (result === 2) {
-
-
-
     $("#language_2").show();
     $("#programing_quiz").hide();
-    $(".btn").click(reset());
     } 
     else if (result === 3) {
-
-
-
     $("#language_3").show()
     $("#programing_quiz").hide();
     }
     event.preventDefault();
+  });
+  $(".btn2").click(function() {
+    $("#language_1").hide();
+    $("#language_2").hide();
+    $("#language_3").hide();
+    $("#programing_quiz").show();
   });
 });
