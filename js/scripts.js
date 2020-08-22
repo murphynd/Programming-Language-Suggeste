7016abf5a7fdcc76b2c67ec87a0f0ec0fd09901a
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-22 15:16:51 -0700

    removing name value in check box for value only to rewrite javaScript

--- js/scripts.js
+++ js/scripts.js
@@ -3,44 +3,73 @@ $(document).ready(function() {
     $("#programing_quiz").show();
     $(".jumbotron").hide();
   });
-  let score = [];
-  $("input:check[name=Ruby]:checked").each(function()){
-    score.push(parseInt($(this).val()));
-  });
-  $("input:check[name=c#]:checked").each(function()){
-    score.push(parseInt($(this).val()));
-  });
-  $("input:check[name=Python]:checked").each(function()){
-    score.push(parseInt($(this).val()));
-  });
 
   $("form#programing_quiz").submit(function(event) {
-    const nameInput = $("#name").val();
-    const result = parseInt($("#question1").val());
-  
-    $(".name").append(nameInput);
-
-    if(result === 1) {
-    $("#language_1").show();
-    $("#programing_quiz").hide();
-    } 
-    else if (result === 2) {
-    $("#language_2").show();
-    $("#programing_quiz").hide();
-    } 
-    else if (result === 3) {
-    $("#language_3").show()
-    $("#programing_quiz").hide();
-    }
     event.preventDefault();
+    
+    let score = [];
+    $("input:check[name=c]:checked").each(function(){
+      score.push(parseInt($(this).val()));
+    });
+    $("input:check[name=Ruby]:checked").each(function(){
+      score.push(parseInt($(this).val()));
+    });
+    $("input:check[name=Python]:checked").each(function(){
+      score.push(parseInt($(this).val()));
+    });
+
+    const totalScore = score.reduce((a,b,c) => a + b + c, 0)
+    console.log(totalScore)
+
+      if (totalScore > num){
+      $("#language_1").show();
+      $("#programing_quiz").hide();
+      } else if (totalScore === 2) {
+        $("#language_2").show();
+        $("#programing_quiz").hide();
+      } else if (totalScore === 3) {
+        $("#language_3").show()
+        $("#programing_quiz").hide();
+      }
+      const nameInput = $("#name").val();
+      $(".name").append(nameInput);
   });
 
+    $(".btn2").click(function() {
+      $("#language_1").hide();
+      $("#language_2").hide();
+      $("#language_3").hide();
+      $("#programing_quiz").trigger("reset").show();
+    });
+ });
+
+//   $("form#programing_quiz").submit(function(event) {
+//     const nameInput = $("#name").val();
+//     const result = parseInt($("#question1").val());
+  
+//     $(".name").append(nameInput);
+
+//     if(result === 1) {
+//     $("#language_1").show();
+//     $("#programing_quiz").hide();
+//     } 
+//     else if (result === 2) {
+//     $("#language_2").show();
+//     $("#programing_quiz").hide();
+//     } 
+//     else if (result === 3) {
+//     $("#language_3").show()
+//     $("#programing_quiz").hide();
+//     }
+//     event.preventDefault();
+//   });
 
 
-  $(".btn2").click(function() {
-    $("#language_1").hide();
-    $("#language_2").hide();
-    $("#language_3").hide();
-    $("#programing_quiz").trigger("reset").show();
-  });
-});
+
+//   $(".btn2").click(function() {
+//     $("#language_1").hide();
+//     $("#language_2").hide();
+//     $("#language_3").hide();
+//     $("#programing_quiz").trigger("reset").show();
+//   });
+// });
