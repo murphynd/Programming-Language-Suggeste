aeafa5c537f0c8f75fed8259df4f484747f9aa15
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-08-24 15:50:49 -0700

    forgotten update

--- js/scripts.js
+++ js/scripts.js
@@ -3,70 +3,31 @@ $(document).ready(function() {
     $("#programing_quiz").show();
     $(".jumbotron").hide();
   });
-
+  
   $("form#programing_quiz").submit(function(event) {
+    const nameInput = $("#name").val();
+    const result = parseInt($("#question1").val());
+  
+    $(".name").append(nameInput);
+
+    if(result === 1) {
+    $("#language_1").show();
+    $("#programing_quiz").hide();
+    } 
+    else if (result === 2) {
+    $("#language_2").show();
+    $("#programing_quiz").hide();
+    } 
+    else if (result === 3) {
+    $("#language_3").show()
+    $("#programing_quiz").hide();
+    }
     event.preventDefault();
-    
-    var score=[]
-    $("input:check[name=skill]:checked").each(function(){
-      score.push($(this).val());
-    });
-    console.log(score);
+  });
+  $(".btn2").click(function() {
+    $("#language_1").hide();
+    $("#language_2").hide();
+    $("#language_3").hide();
+    $("#programing_quiz").trigger("reset").show();
   });
 });
-
-//     const totalScore = score.reduce((a,b,c) => a + b + c, 0)
-//     console.log(totalScore)
-
-//       if (totalScore > num){
-//       $("#language_1").show();
-//       $("#programing_quiz").hide();
-//       } else if (totalScore === 2) {
-//         $("#language_2").show();
-//         $("#programing_quiz").hide();
-//       } else if (totalScore === 3) {
-//         $("#language_3").show()
-//         $("#programing_quiz").hide();
-//       }
-//       const nameInput = $("#name").val();
-//       $(".name").append(nameInput);
-//   });
-
-//     $(".btn2").click(function() {
-//       $("#language_1").hide();
-//       $("#language_2").hide();
-//       $("#language_3").hide();
-//       $("#programing_quiz").trigger("reset").show();
-//     });
-//  });
-
-//   $("form#programing_quiz").submit(function(event) {
-//     const nameInput = $("#name").val();
-//     const result = parseInt($("#question1").val());
-  
-//     $(".name").append(nameInput);
-
-//     if(result === 1) {
-//     $("#language_1").show();
-//     $("#programing_quiz").hide();
-//     } 
-//     else if (result === 2) {
-//     $("#language_2").show();
-//     $("#programing_quiz").hide();
-//     } 
-//     else if (result === 3) {
-//     $("#language_3").show()
-//     $("#programing_quiz").hide();
-//     }
-//     event.preventDefault();
-//   });
-
-
-
-//   $(".btn2").click(function() {
-//     $("#language_1").hide();
-//     $("#language_2").hide();
-//     $("#language_3").hide();
-//     $("#programing_quiz").trigger("reset").show();
-//   });
-// });
